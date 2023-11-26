3801ad822f3374ecbb6cbf0a060078a4b27ee41f
Slight Chan <1595846471@qq.com>
2023-11-27 00:06:27 +0800

    feat: Optimize code indentation

--- web/vue-demo/src/pages/ConfigForm/components/ConfigInput.vue
+++ web/vue-demo/src/pages/ConfigForm/components/ConfigInput.vue
@@ -43,11 +43,11 @@ const initForm = () => {
 <template>
   <div class="flex w-1/2 items-end">
     <el-input
-    v-model="textarea"
-    :autosize="{ minRows: 5 }"
-    type="textarea"
-    placeholder="Please input"
-  />
+      v-model="textarea"
+      :autosize="{ minRows: 5 }"
+      type="textarea"
+      placeholder="Please input"
+    />
     <div class="ml-2 items-end">
       <el-button type="success" plain @click="initForm">生成表单</el-button>
     </div>
